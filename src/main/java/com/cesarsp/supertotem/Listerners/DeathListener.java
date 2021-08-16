144887da7b714c9a24b4ce6e60c682509bc8a784
CesarSP99 <cesarandres00@gmail.com>
2021-08-16 16:12:51 -0400

    Add Listener body

---
+++ src/main/java/com/cesarsp/supertotem/Listerners/DeathListener.java
@@ -0,0 +1,12 @@
+package com.cesarsp.supertotem.Listerners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathListener implements Listener {
+    @EventHandler
+    void onPlayerDeath(PlayerDeathEvent playerDeathEvent){
+
+    }
+}
