bd4e0d6f2081afffc604eb2f5dfafbfa3ee3cdd8
CesarSP99 <cesarandres00@gmail.com>
2021-08-16 19:18:38 -0400

    Add some messages and register listener

--- src/main/java/com/cesarsp/supertotem/SuperTotem.java
+++ src/main/java/com/cesarsp/supertotem/SuperTotem.java
@@ -2,7 +2,6 @@ package com.cesarsp.supertotem;
 
 import com.cesarsp.supertotem.Listerners.DeathListener;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.logging.Logger;
 
 public final class SuperTotem extends JavaPlugin {
@@ -10,8 +9,16 @@ public final class SuperTotem extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         Logger logger = this.getLogger();
-        logger.info("Enabling SuperTotem");
-        getServer().getPluginManager().registerEvents(new DeathListener(), this);
+        logger.info("Trying to initialize SuperTotem...");
+        try{
+            getServer().getPluginManager().registerEvents(new DeathListener(), this);
+            logger.info("SuperTotem was initialized successfully! :D");
+        }
+        catch (Exception e){
+            logger.severe("An error has occurred");
+            logger.severe(e.toString());
+            getPluginLoader().disablePlugin(this);
+        }
     }
 
     @Override
